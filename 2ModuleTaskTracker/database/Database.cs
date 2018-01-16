033cc022dbe66204951e2a74ddf859e253aca7a1
Daniil Maksimov <maksimovdaniil.alex@yandex.ru>
2018-01-17 00:26:42 +0400

    Add abstract and 'User' database classes

---
+++ 2ModuleTaskTracker/database/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2ModuleTaskTracker.database {
+	public class Database<T> {
+		public static Database<T> Read(string filePath) {
+			return new Database<T>();
+		}
+
+		public void Write(string filePath) {
+		}
+
+		public void Add(T entry) { }
+		public void Remove(T entry) { }
+		public void Update(T entry) { }
+		public IEnumerable<T> Select() { return null; }
+	}
+}
---
+++ 2ModuleTaskTracker/database/UserDatable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2ModuleTaskTracker.database {
+	public class UserDatable : Database<User> {
+	}
+}

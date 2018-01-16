7d816134a5dd8352f48665c225cf813e8d1b3632
Unknown <rikoshetru@yandex.ru>
2018-01-17 01:21:24 +0400

    Add authentication

---
+++ CommonLibrary/entities/Authentication.cs
@@ -0,0 +1,33 @@
+﻿using CommonLibrary.database;
+using System.Linq;
+
+namespace CommonLibrary.entities
+{
+    public class Authentication
+    {
+        private UserDatabase database;
+
+        public User CurrentUser { get; private set; }
+
+        public Authentication(UserDatabase database)
+        {
+            this.database = database;
+        }
+
+        public bool Login(string login, string rawPassword)
+        {
+            CurrentUser = null;
+
+            if (!database.Select().Any())
+            {
+                User admin = new User(0, login, rawPassword, User.UserAccessLevel.Admin);
+                database.Add(admin);
+            }
+
+            CurrentUser = database.Select().FirstOrDefault(
+                u => login.Equals(u.Login) && u.IsCorrectPassword(rawPassword));
+            
+            return CurrentUser == null;
+        }
+    }
+}
---
+++ DebugConsoleProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CommonLibrary.database;
+
+namespace DebugConsoleProject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            User user = new User(0, "123", "4", User.UserAccessLevel.Admin);
+            Console.WriteLine(user.IsCorrectPassword("4"));
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

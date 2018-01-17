457e642140c14097eba873e23bfaa852c36741cd
Daniil Maksimov <maksimovdaniil.alex@yandex.ru>
2018-01-17 16:15:26 +0400

    Fix user database password serialization

--- CommonLibrary/entities/User.cs
+++ CommonLibrary/entities/User.cs
@@ -1,37 +1,66 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace CommonLibrary.database {
-	public class User {
-        
-        public string PasswordHash { get; set; }
-        public int ID { get; set; }
-        public string Login { get; set; }
-        public UserAccessLevel AccessLevel { get; set; }
+	public class User : INotifyPropertyChanged {
+		[JsonProperty]
+		[JsonConverter(typeof(PasswordConverter))]
+		private string password;
+		private int id;
+		private string login;
+		private UserAccessLevel accessLevel;
+		public event PropertyChangedEventHandler PropertyChanged;
 
 		private string RawToHash(string raw) {
-			HashAlgorithm md5 = MD5.Create();
-
-			StringBuilder sb = new StringBuilder();
-			foreach(byte b in md5.ComputeHash(Encoding.UTF8.GetBytes(raw)))
-				sb.Append(b.ToString("X2"));
-
-			return sb.ToString();
+			return Encoding.UTF8.GetString(MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(raw)));
 		}
 
+		public int Id {
+			get => id;
+			set {
+				id = value;
+				OnPropertyChanged();
+			}
+		}
+		public string Login {
+			get => login;
+			set {
+				login = value;
+				OnPropertyChanged();
+			}
+		}
+
+		[JsonIgnore]
 		public string Password {
-			set { PasswordHash = RawToHash(value); }
+			get => password;
+			set {
+				password = RawToHash(value);
+				OnPropertyChanged();
+			}
+		}
+
+		[JsonConverter(typeof(StringEnumConverter))]
+		public UserAccessLevel AccessLevel {
+			get => accessLevel;
+			set {
+				accessLevel = value;
+				OnPropertyChanged();
+			}
 		}
 
 		public bool IsCorrectPassword(string rawPassword) {
-			return RawToHash(rawPassword).Equals(PasswordHash);
+			return RawToHash(rawPassword).Equals(password);
 		}
 
-		private User() {}
+		private User() { }
 
 		public User(int id, string login, string rawPassword, UserAccessLevel level) {
-			ID = id;
+			Id = id;
 			Login = login;
 			Password = rawPassword;
 			AccessLevel = level;
@@ -43,6 +72,28 @@ namespace CommonLibrary.database {
 		public enum UserAccessLevel {
 			Default,
 			Admin
+		}
+
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		public class PasswordConverter : JsonConverter {
+			public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+				writer.WriteValue(value as String);
+			}
+
+			public override bool CanRead {
+				get { return true; }
+			}
+
+			public override bool CanConvert(Type objectType) {
+				return objectType == typeof(String);
+			}
+
+			public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+				return reader.Value as String;
+			}
 		}
 	}
 }
